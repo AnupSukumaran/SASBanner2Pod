f9a278a7c35666f4607bbf94e4db5151c50d79a6
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-09-16 17:01:02 +0530

    - added files

---
+++ Example/Pods/Target Support Files/Pods-SASBanner2_Tests/Pods-SASBanner2_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SASBanner2_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SASBanner2_Tests
+@end
---
+++ Example/Pods/Target Support Files/SASBanner2/SASBanner2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SASBanner2 : NSObject
+@end
+@implementation PodsDummy_SASBanner2
+@end
